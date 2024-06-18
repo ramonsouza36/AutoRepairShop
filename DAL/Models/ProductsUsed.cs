d48b6e0cb579c8f75eec406f6d99e0f927f07cc4
RamonSouza <ramonriuller@outlook.com>
2024-06-17 22:03:32 -0300

    Ajuste tela de Ordem de Serviço

---
+++ DAL/Models/ProductsUsed.cs
@@ -0,0 +1,14 @@
+public partial class ProductsUsed
+{
+    public ProductsUsed(){}
+
+    #region Properties
+
+    public string? ProductName { get; set; }
+
+    public int? QuantityUsed { get; set; }
+
+    public decimal? TotalValue { get; set; }
+
+    #endregion Properties
+}
